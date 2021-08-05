ccc5211940f0022eff97e445fe1269955de356c2
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2021-08-04 22:25:45 -0400

    Update EightBall.java

--- src/main/java/com/general_hello/commands/commands/Entertainments/EightBall.java
+++ src/main/java/com/general_hello/commands/commands/Entertainments/EightBall.java
@@ -3,9 +3,8 @@ import java.util.Random;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class EightBall extends ListenerAdapter {
-	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-		String []args = event.getMessage().getContentRaw().split("\\s+");
+public class EightBall {
+	static void eightball(GuildMessageReceivedEvent event, String []args){
 		String [] answerlist = new String[20];
 		answerlist[0] = "It is certain.";
 		answerlist[1] = "It is decidedly so.";
@@ -27,20 +26,14 @@ public class EightBall extends ListenerAdapter {
 		answerlist[17] = "My sources say no.";
 		answerlist[18] = "Outlook not so good.";
 		answerlist[19] = "Very doubtful.";
-		if (args[0].toLowerCase().contains((prefix + "8ball")) && !event.getAuthor().isBot()) {
-			try {
-				if(args.length == 1) {
-					event.getChannel().sendTyping().queue();
-					event.getChannel().sendMessage("Please specify what you want me to answer.").queue();
-				}else {
-					Random rand = new Random();
-					int answer = rand.nextInt(20);
-					event.getChannel().sendTyping().queue();
-					event.getChannel().sendMessage(answerlist[answer]).queue();
-				}
-			}catch(Exception e) {
-				System.out.println("EightBall: " + event.getGuild());
-			}
+		if(args.length == 1) {
+			event.getChannel().sendTyping().queue();
+			event.getChannel().sendMessage("Please specify what you want me to answer.").queue();
+		}else {
+			Random rand = new Random();
+			int answer = rand.nextInt(20);
+			event.getChannel().sendTyping().queue();
+			event.getChannel().sendMessage(answerlist[answer]).queue();
 		}
 	}
 }
