0eb2d5835929f16d1c8e8acb6936bd653c379734
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2021-08-04 22:31:42 -0400

    Update Read.java

--- src/main/java/com/general_hello/commands/commands/Entertainments/Read.java
+++ src/main/java/com/general_hello/commands/commands/Entertainments/Read.java
@@ -8,44 +8,39 @@ import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class Read extends ListenerAdapter {
-	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
-		String[] args = event.getMessage().getContentRaw().split("\\s+");
-		try {
-			if (args[0].equalsIgnoreCase(prefix + "read")) {
-				Message message = event.getMessage();
-				if(message.getAttachments().size() == 0){
-					event.getChannel().sendMessage("Attach a file that you want to compile!").queue();
-				}else{
-					InputStream stream;
-					try {
-						stream = message.getAttachments().get(0).retrieveInputStream().get();
-						ArrayList<Character> arr = new ArrayList<>();
-						int b;
-						while((b = stream.read()) != -1)
-							arr.add((char) b);
-						char[] unboxed = new char[arr.size()];
-						for(int i = 0; i < unboxed.length; i++)
-							unboxed[i] = arr.get(i);
-						String s = String.valueOf(unboxed);
-						EmbedBuilder text = new EmbedBuilder()
-								.setTitle("Recieved a file! \nBytes read:")
-								.setColor(0x7289DA)
-								.setDescription(s);
-						event.getChannel().sendMessage(text.build()).queue();
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					} catch (ExecutionException e) {
-						e.printStackTrace();
-					} catch (IOException e) {
-						e.printStackTrace();
-					} catch (Exception e) {
-						event.getChannel().sendMessage("*Error, Looks like your document has more than 2048 characters!*").queue();
-					}
+public class Read {
+	static void read(GuildMessageReceivedEvent event, String []args){
+		if (args[0].equalsIgnoreCase(prefix + "read")) {
+			Message message = event.getMessage();
+			if(message.getAttachments().size() == 0){
+				event.getChannel().sendMessage("Attach a file that you want to read!").queue();
+			}else{
+				InputStream stream;
+				try {
+					stream = message.getAttachments().get(0).retrieveInputStream().get();
+					ArrayList<Character> arr = new ArrayList<>();
+					int b;
+					while((b = stream.read()) != -1)
+						arr.add((char) b);
+					char[] unboxed = new char[arr.size()];
+					for(int i = 0; i < unboxed.length; i++)
+						unboxed[i] = arr.get(i);
+					String s = String.valueOf(unboxed);
+					EmbedBuilder text = new EmbedBuilder()
+							.setTitle("Recieved a file! \nBytes read:")
+							.setColor(0x7289DA)
+							.setDescription(s);
+					event.getChannel().sendMessage(text.build()).queue();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} catch (ExecutionException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				} catch (Exception e) {
+					event.getChannel().sendMessage("*Error, Looks like your document has more than 2048 characters!*").queue();
 				}
 			}
-		}catch(Exception e) {
-			System.out.println("Read: " + event.getGuild());
 		}
 	}
 }
