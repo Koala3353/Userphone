cf3b467f450b1eca53949940856c267d31b25b55
itsmarsss <88464166+itsmarsss@users.noreply.github.com>
2021-08-04 22:28:36 -0400

    Update Poll.java

--- src/main/java/com/general_hello/commands/commands/Entertainments/Poll.java
+++ src/main/java/com/general_hello/commands/commands/Entertainments/Poll.java
@@ -2,36 +2,31 @@ import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
-public class Poll extends ListenerAdapter{
-	public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
+public class Poll {
+	static void poll(GuildMessageReceivedEvent event, String []args){
 		String [] args = event.getMessage().getContentRaw().split("\\s+");
-		try {
-			if (args[0].equalsIgnoreCase(prefix + "poll")){
-				event.getMessage().addReaction("✅").queue();
-				event.getMessage().addReaction("❌").queue();
-				event.getMessage().addReaction("➖").queue();
-			}
-			if (args[0].equalsIgnoreCase("=pollnew")){
-				String question = "";
-				for(int i = 1; i < args.length; i++) {
-					question = question + " " + args[i];
-				}
-				event.getChannel().sendTyping().queue();
-				event.getChannel().sendMessage("VOTE NOW").queue();
-				EmbedBuilder poll = new EmbedBuilder()
-						.setTitle("POLL")
-						.setColor(0x7289DA)
-						.setDescription(question);
-				event.getChannel().sendTyping().queue();
-				event.getChannel().sendMessage(poll.build()).queue(message -> {
-					message.addReaction("✅").queue();
-					message.addReaction("❌").queue();
-					message.addReaction("➖").queue();
-				});
-				poll.clear();
-			}
-		}catch(Exception e) {
-			System.out.println("Poll: " + event.getGuild());
+		event.getMessage().addReaction("✅").queue();
+		event.getMessage().addReaction("❌").queue();
+		event.getMessage().addReaction("➖").queue();
+	}
+	
+	static void newpoll(GuildMessageReceivedEvent event, String []args){
+		String question = "";
+		for(int i = 1; i < args.length; i++) {
+			question = question + " " + args[i];
 		}
+		event.getChannel().sendTyping().queue();
+		event.getChannel().sendMessage("VOTE NOW").queue();
+		EmbedBuilder poll = new EmbedBuilder()
+				.setTitle("POLL")
+				.setColor(0x7289DA)
+				.setDescription(question);
+		event.getChannel().sendTyping().queue();
+		event.getChannel().sendMessage(poll.build()).queue(message -> {
+			message.addReaction("✅").queue();
+			message.addReaction("❌").queue();
+			message.addReaction("➖").queue();
+		});
+		poll.clear();
 	}
 }
