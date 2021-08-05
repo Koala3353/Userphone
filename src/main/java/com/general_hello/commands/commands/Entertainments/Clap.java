803c160896a6eb8b95de3685977bb5730eb0fd61
itsmarsss <itsmarzzzzzz@gmail.com>
2021-08-05 00:08:32 -0400

    test

--- src/main/java/com/general_hello/commands/commands/Entertainments/Clap.java
+++ src/main/java/com/general_hello/commands/commands/Entertainments/Clap.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 public class Clap {
 	static void clap(GuildMessageReceivedEvent event, String []args){
+		//test
 		String message = "👏";
 		for(int i = 1; i < args.length; i++) {
 			message = message + args[i] + "👏";
