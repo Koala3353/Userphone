05ff0e80b8c51c5f7bfb0e9805f9c6bfb9537e93
General Hello <keenebrigado@gmail.com>
2021-09-05 10:30:09 +0800

    Fixed bj

--- src/main/java/com/general_hello/commands/commands/GroupOfGames/Blackjack/BlackjackCommand.java
+++ src/main/java/com/general_hello/commands/commands/GroupOfGames/Blackjack/BlackjackCommand.java
@@ -1,15 +1,12 @@
 package com.general_hello.commands.commands.GroupOfGames.Blackjack;
 
 import com.general_hello.commands.commands.CommandContext;
-import com.general_hello.commands.commands.GetData;
 import com.general_hello.commands.commands.ICommand;
 import com.general_hello.commands.commands.RankingSystem.LevelPointManager;
-import com.general_hello.commands.commands.Utils.MoneyData;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.User;
 
 import java.sql.SQLException;
-import java.text.DecimalFormat;
 
 public class BlackjackCommand implements ICommand {
     private GameHandler gameHandler;
@@ -23,29 +20,17 @@ public class BlackjackCommand implements ICommand {
     public void handle(CommandContext e) throws InterruptedException, SQLException {
         User author = e.getAuthor();
         long playerId = author.getIdLong();
-        int bet = e.getArgs().size() == 0 ? 0 : getInt(e.getArgs().get(0));
-        if (e.getArgs().isEmpty()) {
-            e.getChannel().sendMessage("Are you gonna bet on nothing!!!!").queue();
-            return;
-        }
 
-        if (e.getArgs().size() != 0 && e.getArgs().get(0).matches("(?i)all(-?in)?")) {
-            bet = (int) Math.round(GetData.getLevelPoints(author));
-        }
-        if (bet >= 10) {
-            if (GetData.getLevelPoints(author) - bet >= 0) {
-                BlackjackGame objg = GameHandler.getBlackJackGame(playerId);
+         BlackjackGame objg = GameHandler.getBlackJackGame(playerId);
                 if (objg == null) {
-                    BlackjackGame bjg = new BlackjackGame(bet);
+                    BlackjackGame bjg = new BlackjackGame(50);
                     EmbedBuilder eb = bjg.buildEmbed(author.getName(), e.getGuild());
                     if (!bjg.hasEnded()) {
                         GameHandler.putBlackJackGame(playerId, bjg);
                     } else {
                         double d = bjg.getWonCreds();
                         LevelPointManager.feed(author, (long) d);
-                        int credits = (int) Math.round(MoneyData.money.get(author));
-                        DecimalFormat formatter = new DecimalFormat("#,###.00");
-                        eb.addField("Credits", String.format("You now have %s credits", formatter.format(credits)), false);
+                        eb.addField("XP points", "You now have more XP points", false);
                     }
                     e.getChannel().sendMessageEmbeds(eb.build()).queue(m -> {
                         if (!bjg.hasEnded()) bjg.setMessageId(m.getIdLong());
@@ -53,13 +38,7 @@ public class BlackjackCommand implements ICommand {
                 } else {
                     e.getChannel().sendMessage("You're already playing a game").queue();
                 }
-            } else {
-                e.getChannel().sendMessage(String.format("You don't have enough credits to make a %d credits bet", bet)).queue();
             }
-        } else {
-            e.getChannel().sendMessage("You need to place a bet for at least 10 credits").queue();
-        }
-    }
 
     @Override
     public String getName() {
